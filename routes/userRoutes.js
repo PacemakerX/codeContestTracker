7c46e225e951ee7e187b0142ae9343051afe0431
PacemakerX <sparsh.officialwork@gmail.com>
2025-03-15 12:42:12 +0530

    adds updation routes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,9 +3,12 @@ const {
   registerUser,
   loginUser,
   getUserProfile,
+  updateUserProfile
 } = require("../controllers/userController");
 
 const router = express.Router();
+
+const authenticate = require("../middleware/authMiddleware");
 
 // Register a new user
 router.post("/register", registerUser);
@@ -14,6 +17,8 @@ router.post("/register", registerUser);
 router.post("/login", loginUser);
 
 // Get user profile (protected route)
-router.get("/profile", getUserProfile);
+router.get("/profile", authenticate,getUserProfile);
+
+router.put("/update", authenticate,updateUserProfile);
 
 module.exports = router;
