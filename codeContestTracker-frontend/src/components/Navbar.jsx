4b2410f963660ad4e6b49fcacd922dc87062d6e1
PacemakerX <sparsh.officialwork@gmail.com>
2025-03-24 21:59:24 +0530

    makes navbar transparent, makes it cool

--- codeContestTracker-frontend/src/components/Navbar.jsx
+++ codeContestTracker-frontend/src/components/Navbar.jsx
@@ -19,7 +19,7 @@ export default function Navbar() {
   };
 
   return (
-    <Disclosure as="nav" className="bg-gray-800">
+    <Disclosure as="nav" className="">
       <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
         <div className="relative flex h-16 items-center justify-between">
           <div className="flex items-center">
