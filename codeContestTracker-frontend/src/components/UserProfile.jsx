077855df8a7286372ff79d72c5f1e5b59ead5733
PacemakerX <sparsh.officialwork@gmail.com>
2025-03-24 22:15:22 +0530

    removes unnecessary style while loading

--- codeContestTracker-frontend/src/components/UserProfile.jsx
+++ codeContestTracker-frontend/src/components/UserProfile.jsx
@@ -37,7 +37,7 @@ const UserProfile = () => {
 
   if (loading) {
     return (
-      <div className="flex justify-center items-center h-screen bg-gradient-to-r from-gray-900 to-gray-950">
+      <div className="flex justify-center items-center">
         <div className="animate-spin rounded-full h-16 w-16 border-t-4 border-blue-500"></div>
       </div>
     );
