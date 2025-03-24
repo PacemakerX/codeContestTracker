9df502a26e592b013c7fd64c8517d86c303bfd7d
PacemakerX <sparsh.officialwork@gmail.com>
2025-03-24 22:22:16 +0530

    adds a about route

--- codeContestTracker-frontend/src/App.jsx
+++ codeContestTracker-frontend/src/App.jsx
@@ -6,13 +6,14 @@ import Footer from "./components/Footer"; // Adjust path if needed
 import Login from "./components/Login";
 import Signup from "./components/SignUp";
 import UserProfile from "./components/UserProfile";
+import About from "./components/About";
 import { NotificationProvider } from "./components/ToastNotification"; // Import the provider
 
 function App() {
   return (
     <NotificationProvider> {/* Wrap everything with NotificationProvider */}
       <Router>
-        <div className="flex flex-col min-h-screen bg-gradient-to-br from-gray-900 to-black text-white">
+        <div className="flex flex-col min-h-screen bg-gradient-to-br from-gray-900 via-black to-gray-900 text-white">
           <Navbar />
           <section className="mb-3 text-center px-4 py-5">
             <h1 className="text-4xl font-bold text-white mb-4">
@@ -28,6 +29,7 @@ function App() {
               <Route path="/login" element={<Login />} />
               <Route path="/signup" element={<Signup />} />
               <Route path="/profile" element={<UserProfile/>} />
+              <Route path="/about" element={<About/>} />
             </Routes>
           </main>
           
