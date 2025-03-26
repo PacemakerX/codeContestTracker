346318e3791ad9306fd34a52bede74e39d2772e6
PacemakerX <sparsh.officialwork@gmail.com>
2025-03-26 14:55:03 +0530

    adds a route for contact

--- codeContestTracker-backend/routes/userRoutes.js
+++ codeContestTracker-backend/routes/userRoutes.js
@@ -7,6 +7,7 @@ const {
 } = require("../controllers/userController");
 const router = express.Router();
 const OTPController = require("../controllers/otpController.js");
+const ContactController = require("../controllers/contactController");
 
 const authenticate = require("../middleware/authMiddleware");
 // Route for user registration
@@ -34,5 +35,10 @@ router.post("/send-otp", OTPController.sendOTP);
 // POST /api/users/verify-otp
 // Public access - validates the OTP entered by user
 router.post("/verify-otp", OTPController.verifyOTP);
+
+// Route to contact form submission
+// POST /api/users/contact
+// Public access - sends user's message to site admin
+router.post("/contact", ContactController.submitContactForm);
 
 module.exports = router;
